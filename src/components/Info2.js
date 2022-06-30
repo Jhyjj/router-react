ac2321b2bab6497afbc6a5d2ec3c18524c364e6c
jhj <jyj3768@gmail.com>
2022-06-30 17:03:43 +0900

    first commit

---
+++ src/components/Info2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Info2 = (props) => {
+    return (
+        <div>
+            정보서브 컴포넌트 2번입니다.
+        </div>
+    );
+};
+
+export default Info2;
---
+++ src/components/info.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import Info1 from './Info1';
+import Info2 from './Info2';
+
+
+const Info = (props) => {
+    return (
+        <div>
+            정보페이지 입니다.
+            <div>
+                <Link to="info1">정보1</Link>
+                <Link to="info2">정보2</Link>  
+            </div>
+            <Routes>
+                <Route path='info1' element={<Info1/>}/>
+                <Route path='info2' element={<Info2/>}/>
+                <Route/>
+            </Routes>
+        </div>
+    );
+};
+
+export default Info;
